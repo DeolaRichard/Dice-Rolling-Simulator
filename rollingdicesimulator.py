4be72c25842349f580be0edc94da3ea6b99747c6
DeolaRichard <adekunlerichie5@gmail.com>
2023-08-01 07:43:03 +0100

    Commented on the codes to give good descriptions of the functions

--- rollingdicesimulator.py
+++ rollingdicesimulator.py
@@ -48,17 +48,17 @@ def roll_dice():
 
     }
 
-    roll = input("Roll the dice? (Yes/No): ").lower()
+    roll = input("Roll the dice? (Yes/No): ").lower()  # makes the user input case insensitive
 
     while roll.lower() == "Yes".lower():
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
+        dice1 = random.randint(1, 6)  # gets a random integer number within the range 1 to 6 and assigns it dice 1
+        dice2 = random.randint(1, 6)  # gets a random integer number within the range 1 to 6 and assigns to dice 2
 
         print("Dice rolled: {} and {}".format(dice1, dice2))
-        print("\n".join(dice_drawing[dice1]))
-        print("\n".join(dice_drawing[dice2]))
+        print("\n".join(dice_drawing[dice1]))  # joins the dictionary to dice 1
+        print("\n".join(dice_drawing[dice2]))  # joins the dictionary to dice 2
 
-        roll = input("Roll again? (Yes/NO): ")
+        roll = input("Roll again? (Yes/NO): ")  # asks the user to roll again
 
 
 roll_dice()
